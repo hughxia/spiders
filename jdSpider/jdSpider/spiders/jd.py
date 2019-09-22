197ad53015d6038e15c68c9689427814d45c8ba1
hugh <xsmlinzi@163.com>
2019-09-22 23:52:26 +0800

    get product_id

--- jdSpider/jdSpider/spiders/jd.py
+++ jdSpider/jdSpider/spiders/jd.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+import re
+
 
 class JdSpider(scrapy.Spider):
     name = 'jd'
     allowed_domains = ['jd.com']
-    start_urls = ['http://jd.com/']
+    start_urls = ['https://item.jd.com/100005981268.html']
 
     def parse(self, response):
-        pass
+        for url in self.start_urls:
+            product_id = re.findall(r"com/(.*?).html", url)[0]
+            print("product_id:", product_id)
+            pass
