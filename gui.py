cdb250d1be414b6f7644aba7684ae754dc45547b
hugh <v_xiashengmiao.js@chinatelecom.cn>
2019-09-25 21:29:48 +0800

    complete first gui by tkinter

--- gui.py
+++ gui.py
@@ -5,8 +5,22 @@ window = tk.Tk()
 window.title("my window")
 window.geometry('200x100')
 
-lbl = tk.Label(window, text='H-H', bg='blue', font=('Arial', 12),
+var = tk.StringVar()
+lbl = tk.Label(window, textvariable=var, bg='green', font=('Arial', 12),
             width=12, height=2)
 lbl.pack()
+
+on_hit = False
+
+def hit_me():
+    global on_hit
+    if not on_hit:
+        var.set('you hit me')
+    else:
+        var.set('')
+    on_hit = not on_hit
+
+btn = tk.Button(window, text='hit me', width=8, height=2, command=hit_me)
+btn.pack()
 
 window.mainloop()
